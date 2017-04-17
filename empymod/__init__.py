d9b563bc5eead7f3369ee96ff8a8394e8068628a
Dieter Werthmüller <mail@werthmuller.org>
2017-04-17 12:17:13 -0500

    Add note regarding scipy 0.19.0 quad-mem-issue

--- empymod/__init__.py
+++ empymod/__init__.py
@@ -31,6 +31,12 @@ Required are python version 3.4 or higher and the modules `NumPy` and `SciPy`.
 If you want to run parts of the kernel in parallel, the module `numexpr` is
 required additionally.
 
+**Note**: Do not use `scipy == 0.19.0`. It has a memory leak in `quad`, see
+`github.com/scipy/scipy/pull/7216 <https://github.com/scipy/scipy/pull/7216>`_.
+So if you use QUAD (or potentially QWE) in any of your transforms you might see
+your memory usage going through the roof.
+
+
 If you are new to Python I recommend using a Python distribution, which will
 ensure that all dependencies are met, specifically properly compiled versions
 of `NumPy` and `SciPy`; I recommend using Anaconda (version 3.x;
