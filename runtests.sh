2f7afb98c9f5341ca2e2351c89545d524f2ccbf1
Dieter Werthmüller <mail@werthmuller.org>
2017-11-10 19:19:09 -0600

    Add -p option

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@ usage="
 $(basename "$0") [-h] [-c -v]
 
 Run pytest for empymod locally in an isolated venv before submitting to
-GitHub/Travis-CI; by default for all supported python versions by empymod.
+GitHub/Travis-CI; by default for all supported python versions of empymod.
 
 where:
     -h : show this help text
@@ -13,14 +13,16 @@ where:
          Default: 4 5 6
     -c : Anaconda channel, e.g. '-c conda-forge' to use conda-forge
          Default: defaults
+    -p : If provided prints output of conda.
 "
 
 # Set default values
 CHANNEL=defaults
 PYTHON3VERSION="4 5 6"
+PRINT=0
 
 # Get Optional Input
-while getopts ":hv:c:" opt; do
+while getopts "hpv:c:" opt; do
   case $opt in
     h) echo "$usage"
        exit
@@ -28,6 +30,8 @@ while getopts ":hv:c:" opt; do
     v) PYTHON3VERSION=$OPTARG
        ;;
     c) CHANNEL=$OPTARG
+       ;;
+    p) PRINT=1
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -58,14 +62,23 @@ for i in ${PYTHON3VERSION[@]}; do
   printf "\e[0m\n"
 
   # Create venv
-  conda create -y -n test_3${i} python=3.${i} &> /dev/null
+  if [ "$PRINT" == "1" ]; then
+    conda create -y -n test_3${i} python=3.${i}
+  else
+    conda create -y -n test_3${i} python=3.${i} &> /dev/null
+  fi
 
   # Activate venv
   source activate test_3${i}
 
   # Install with CHANNEL
-  conda install -c $CHANNEL numpy scipy numexpr python-dateutil setuptools \
-      pytest pytest-cov &> /dev/null
+  if [ "$PRINT" == "1" ]; then
+    conda install -y -c $CHANNEL numpy scipy numexpr python-dateutil \
+        setuptools pytest pytest-cov
+  else
+    conda install -y -c $CHANNEL numpy scipy numexpr python-dateutil \
+        setuptools pytest pytest-cov &> /dev/null
+  fi
 
   # Run tests
   pytest tests/ --cov=empymod
@@ -74,6 +87,10 @@ for i in ${PYTHON3VERSION[@]}; do
   source deactivate test_3${i}
 
   # Remove venv
-  conda remove -y -n test_3${i} --all &> /dev/null
+  if [ "$PRINT" == "1" ]; then
+    conda remove -y -n test_3${i} --all
+  else
+    conda remove -y -n test_3${i} --all &> /dev/null
+  fi
 
 done
