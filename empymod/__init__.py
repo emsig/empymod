emsig/empymod

commit 78647abf76e9f6369740a146caec0b31337299b7

Fix hopefully flake8 W605

empymod/__init__.py
@@ -1,4 +1,4 @@
-"""
+r"""
 
 Theory
 ------

empymod/kernel.py
@@ -1,4 +1,4 @@
-"""
+r"""
 
 :mod:`kernel` -- Kernel calculation
 ===================================
@@ -46,7 +46,7 @@ __all__ = ['wavenumber', 'angle_factor', 'fullspace', 'greenfct',
 
 def wavenumber(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
                ab, xdirect, msrc, mrec, use_ne_eval):
-    """Calculate wavenumber domain solution.
+    r"""Calculate wavenumber domain solution.
 
     Return the wavenumber domain solutions ``PJ0``, ``PJ1``, and ``PJ0b``,
     which have to be transformed with a Hankel transform to the frequency
@@ -131,7 +131,7 @@ def wavenumber(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
 
 def greenfct(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
              ab, xdirect, msrc, mrec, use_ne_eval):
-    """Calculate Green's function for TM and TE.
+    r"""Calculate Green's function for TM and TE.
 
     .. math:: \\tilde{g}^{tm}_{hh}, \\tilde{g}^{tm}_{hz},
               \\tilde{g}^{tm}_{zh}, \\tilde{g}^{tm}_{zz},
@@ -314,7 +314,7 @@ def greenfct(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
 
 
 def reflections(depth, e_zH, Gam, lrec, lsrc, use_ne_eval):
-    """Calculate Rp, Rm.
+    r"""Calculate Rp, Rm.
 
     .. math:: R^\pm_n, \\bar{R}^\pm_n
 
@@ -403,7 +403,7 @@ def reflections(depth, e_zH, Gam, lrec, lsrc, use_ne_eval):
 
 
 def fields(depth, Rp, Rm, Gam, lrec, lsrc, zsrc, ab, TM, use_ne_eval):
-    """Calculate Pu+, Pu-, Pd+, Pd-.
+    r"""Calculate Pu+, Pu-, Pd+, Pd-.
 
     .. math:: P^{u\pm}_s, P^{d\pm}_s, \\bar{P}^{u\pm}_s, \\bar{P}^{d\pm}_s;
           P^{u\pm}_{s-1}, P^{u\pm}_n, \\bar{P}^{u\pm}_{s-1}, \\bar{P}^{u\pm}_n;
@@ -568,7 +568,7 @@ def fields(depth, Rp, Rm, Gam, lrec, lsrc, zsrc, ab, TM, use_ne_eval):
 # Angle Factor
 
 def angle_factor(angle, ab, msrc, mrec):
-    """Return the angle-dependent factor.
+    r"""Return the angle-dependent factor.
 
     The whole calculation in the wavenumber domain is only a function of the
     distance between the source and the receiver, it is independent of the
@@ -624,7 +624,7 @@ def angle_factor(angle, ab, msrc, mrec):
 
 def fullspace(off, angle, zsrc, zrec, etaH, etaV, zetaH, zetaV, ab, msrc,
               mrec):
-    """Analytical full-space solutions in the frequency domain.
+    r"""Analytical full-space solutions in the frequency domain.
 
     .. math:: \\hat{G}^{ee}_{\\alpha\\beta}, \\hat{G}^{ee}_{3\\alpha},
               \\hat{G}^{ee}_{33}, \\hat{G}^{em}_{\\alpha\\beta},
@@ -795,7 +795,7 @@ def fullspace(off, angle, zsrc, zrec, etaH, etaV, zetaH, zetaV, ab, msrc,
 
 def halfspace(off, angle, zsrc, zrec, etaH, etaV, freqtime, ab, signal,
               solution='dhs'):
-    """Return frequency- or time-space domain VTI half-space solution.
+    r"""Return frequency- or time-space domain VTI half-space solution.
 
     Calculates the frequency- or time-space domain electromagnetic response for
     a half-space below air using the diffusive approximation, as given in
@@ -982,16 +982,16 @@ def halfspace(off, angle, zsrc, zrec, etaH, etaV, freqtime, ab, signal,
 
         # Bessel functions for airwave
         def BI(gamH, hp, nr, xim):
-            """Return BI_nr."""
+            r"""Return BI_nr."""
             return np.exp(-np.real(gamH)*hp)*special.ive(nr, xim)
 
         def BK(xip, nr):
-            """Return BK_nr."""
+            r"""Return BK_nr."""
             return np.exp(-1j*np.imag(xip))*special.kve(nr, xip)
 
         # Airwave calculation
         def airwave(s, hp, rp, res, fab, delta):
-            """Return airwave."""
+            r"""Return airwave."""
             # Parameters
             zeta = s*mu_0
             gamH = np.sqrt(zeta/res)
@@ -1029,7 +1029,7 @@ def halfspace(off, angle, zsrc, zrec, etaH, etaV, freqtime, ab, signal,
             fn = np.vectorize(np.math.factorial)
 
             def coeff_dk(k, K):
-                """Return coefficients Dk for k, K."""
+                r"""Return coefficients Dk for k, K."""
                 n = np.arange(int((k+1)/2), np.min([k, K/2])+.5, 1, dtype=int)
                 Dk = n**(K/2)*fn(2*n)
                 Dk /= fn(n)*fn(n-1)*fn(k-n)*fn(2*n-k)*fn(K/2-n)

commit 96c0d16c124ee490f39eba6652f94b3909281a1b

Fix LaTeX-failures
The fix for W605 in #78647abf (replace """..""" by r"""..""") broke
many LaTeX-strings, where double backslashes where used, e.g.,
\\frac. Replaced all double-backslashes by backslashes.

empymod/__init__.py
@@ -368,10 +368,10 @@ part,
 
 .. math::
 
-    E(r, t)^\\text{Impulse} &= \ \\frac{2}{\pi}\int^\infty_0 \Re[E(r, \omega)]\
-                        \cos(\omega t)\ \\text{d}\omega \ , \\\\
-            &= -\\frac{2}{\pi}\int^\infty_0 \Im[E(r, \omega)]\
-                \sin(\omega t)\ \\text{d}\omega \ ,
+    E(r, t)^\text{Impulse} &= \ \frac{2}{\pi}\int^\infty_0 \Re[E(r, \omega)]\
+                        \cos(\omega t)\ \text{d}\omega \ , \\
+            &= -\frac{2}{\pi}\int^\infty_0 \Im[E(r, \omega)]\
+                \sin(\omega t)\ \text{d}\omega \ ,
 
 see, e.g., [Anderson_1975]_ or [Key_2012]_. Quadrature-with-extrapolation,
 FFTLog, and obviously the sine/cosine-transform all make use of this split.
@@ -383,10 +383,10 @@ step-response,
 
 .. math::
 
-    E(r, t)^\\text{Impulse} =
-                        \\frac{\partial\ E(r, t)^\\text{step}}{\partial t}\ .
+    E(r, t)^\text{Impulse} =
+                        \frac{\partial\ E(r, t)^\text{step}}{\partial t}\ .
 
-Using :math:`\\frac{\partial}{\partial t} \Leftrightarrow i\omega` and going
+Using :math:`\frac{\partial}{\partial t} \Leftrightarrow i\omega` and going
 the other way, from impulse to step, leads to the divison by :math:`i\omega`.
 (This only holds because we define in accordance with the causality principle
 that :math:`E(r, t \le 0) = 0`).
@@ -405,17 +405,17 @@ For completeness sake, the step-on response is given by
 
 .. math::
 
-    E(r, t)^\\text{Step-on} = - \\frac{2}{\pi}\int^\infty_0
-                            \Im\\left[\\frac{E(r,\omega)}{i \omega}\\right]\
-                            \sin(\omega t)\ \\text{d}\omega \ ,
+    E(r, t)^\text{Step-on} = - \frac{2}{\pi}\int^\infty_0
+                            \Im\left[\frac{E(r,\omega)}{i \omega}\right]\
+                            \sin(\omega t)\ \text{d}\omega \ ,
 
 and the step-off by
 
 .. math::
 
-    E(r, t)^\\text{Step-off} = - \\frac{2}{\pi}\int^\infty_0
-                             \Re\\left[\\frac{E(r,\omega)}{i\omega}\\right]\
-                             \cos(\omega t)\ \\text{d}\omega \ .
+    E(r, t)^\text{Step-off} = - \frac{2}{\pi}\int^\infty_0
+                             \Re\left[\frac{E(r,\omega)}{i\omega}\right]\
+                             \cos(\omega t)\ \text{d}\omega \ .
 
 
 Note on speed, memory, and accuracy
@@ -538,7 +538,7 @@ can use the benchmark-notebook in the `empymod/example-notebooks
 :math:`\Gamma` (below eq. 19); :math:`W^{u, d}_n` (eq. 74), :math:`r^\pm_n`
 (eq. 65); :math:`R^\pm_n` (eq. 64); :math:`P^{u, d; \pm}_s` (eq. 81);
 :math:`M_s` (eq. 82), and their corresponding bar-ed versions provided in the
-appendix (e.g. :math:`\\bar{\Gamma}`). In big models, more than 95 % of the
+appendix (e.g. :math:`\bar{\Gamma}`). In big models, more than 95 % of the
 calculation is spent in the calculation of these six equations, and most of the
 time therefore in ``np.sqrt`` and ``np.exp``, or generally in
 ``numpy``-``ufuncs`` which are implemented and executed in compiled C-code. For

empymod/kernel.py
@@ -63,7 +63,7 @@ def wavenumber(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
     Bessel functions of order 0 and 1 only. This is achieved by applying the
     recurrence formula
 
-    .. math:: J_2(kr) = \\frac{2}{kr} J_1(kr) - J_0(kr) \ .
+    .. math:: J_2(kr) = \frac{2}{kr} J_1(kr) - J_0(kr) \ .
 
 
     .. note::
@@ -133,9 +133,9 @@ def greenfct(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
              ab, xdirect, msrc, mrec, use_ne_eval):
     r"""Calculate Green's function for TM and TE.
 
-    .. math:: \\tilde{g}^{tm}_{hh}, \\tilde{g}^{tm}_{hz},
-              \\tilde{g}^{tm}_{zh}, \\tilde{g}^{tm}_{zz},
-              \\tilde{g}^{te}_{hh}, \\tilde{g}^{te}_{zz}
+    .. math:: \tilde{g}^{tm}_{hh}, \tilde{g}^{tm}_{hz},
+              \tilde{g}^{tm}_{zh}, \tilde{g}^{tm}_{zz},
+              \tilde{g}^{te}_{hh}, \tilde{g}^{te}_{zz}
 
     This function corresponds to equations 108--110, 117/118, 122; 89--94,
     A18--A23, B13--B15; 97--102 A26--A31, and B16--B18 in
@@ -316,7 +316,7 @@ def greenfct(zsrc, zrec, lsrc, lrec, depth, etaH, etaV, zetaH, zetaV, lambd,
 def reflections(depth, e_zH, Gam, lrec, lsrc, use_ne_eval):
     r"""Calculate Rp, Rm.
 
-    .. math:: R^\pm_n, \\bar{R}^\pm_n
+    .. math:: R^\pm_n, \bar{R}^\pm_n
 
     This function corresponds to equations 64/65 and A-11/A-12 in
     [Hunziker_et_al_2015]_, and loosely to the corresponding files ``Rmin.F90``
@@ -405,9 +405,9 @@ def reflections(depth, e_zH, Gam, lrec, lsrc, use_ne_eval):
 def fields(depth, Rp, Rm, Gam, lrec, lsrc, zsrc, ab, TM, use_ne_eval):
     r"""Calculate Pu+, Pu-, Pd+, Pd-.
 
-    .. math:: P^{u\pm}_s, P^{d\pm}_s, \\bar{P}^{u\pm}_s, \\bar{P}^{d\pm}_s;
-          P^{u\pm}_{s-1}, P^{u\pm}_n, \\bar{P}^{u\pm}_{s-1}, \\bar{P}^{u\pm}_n;
-          P^{d\pm}_{s+1}, P^{d\pm}_n, \\bar{P}^{d\pm}_{s+1}, \\bar{P}^{d\pm}_n
+    .. math:: P^{u\pm}_s, P^{d\pm}_s, \bar{P}^{u\pm}_s, \bar{P}^{d\pm}_s;
+          P^{u\pm}_{s-1}, P^{u\pm}_n, \bar{P}^{u\pm}_{s-1}, \bar{P}^{u\pm}_n;
+          P^{d\pm}_{s+1}, P^{d\pm}_n, \bar{P}^{d\pm}_{s+1}, \bar{P}^{d\pm}_n
 
     This function corresponds to equations 81/82, 95/96, 103/104, A-8/A-9,
     A-24/A-25, and A-32/A-33 in [Hunziker_et_al_2015]_, and loosely to the
@@ -626,9 +626,9 @@ def fullspace(off, angle, zsrc, zrec, etaH, etaV, zetaH, zetaV, ab, msrc,
               mrec):
     r"""Analytical full-space solutions in the frequency domain.
 
-    .. math:: \\hat{G}^{ee}_{\\alpha\\beta}, \\hat{G}^{ee}_{3\\alpha},
-              \\hat{G}^{ee}_{33}, \\hat{G}^{em}_{\\alpha\\beta},
-              \\hat{G}^{em}_{\\alpha 3}
+    .. math:: \hat{G}^{ee}_{\alpha\beta}, \hat{G}^{ee}_{3\alpha},
+              \hat{G}^{ee}_{33}, \hat{G}^{em}_{\alpha\beta},
+              \hat{G}^{em}_{\alpha 3}
 
     This function corresponds to equations 45--50 in [Hunziker_et_al_2015]_,
     and loosely to the corresponding files ``Gin11.F90``, ``Gin12.F90``,

empymod/scripts/fdesign.py
@@ -74,84 +74,84 @@ Implemented Hankel transforms
 
   .. math::
 
-      \\int^\\infty_0 l \\exp\\left(-al^2\\right) J_0(lr) dl =
-      \\frac{\\exp\\left(\\frac{-r^2}{4a}\\right)}{2a}
+      \int^\infty_0 l \exp\left(-al^2\right) J_0(lr) dl =
+      \frac{\exp\left(\frac{-r^2}{4a}\right)}{2a}
 
 - ``j0_2`` [Anderson_1975]_
 
   .. math::
 
-      \\int^\\infty_0 \\exp\\left(-al\\right) J_0(lr) dl =
-      \\frac{1}{\\sqrt{a^2+r^2}}
+      \int^\infty_0 \exp\left(-al\right) J_0(lr) dl =
+      \frac{1}{\sqrt{a^2+r^2}}
 
 - ``j0_3`` [Guptasarma_and_Singh_1997]_
 
   .. math::
 
-      \\int^\\infty_0 l\\exp\\left(-al\\right) J_0(lr) dl =
-      \\frac{a}{(a^2 + r^2)^{3/2}}
+      \int^\infty_0 l\exp\left(-al\right) J_0(lr) dl =
+      \frac{a}{(a^2 + r^2)^{3/2}}
 
 - ``j0_4`` [Chave_and_Cox_1982]_
 
   .. math::
 
-      \\int^\\infty_0 \\frac{l}{\\beta} \\exp\\left(-\\beta z_v \\right)
+      \int^\infty_0 \frac{l}{\beta} \exp\left(-\beta z_v \right)
       J_0(lr) dl =
-      \\frac{\\exp\\left(-\\gamma R\\right)}{R}
+      \frac{\exp\left(-\gamma R\right)}{R}
 
 - ``j0_5`` [Chave_and_Cox_1982]_
 
   .. math::
 
-    \\int^\\infty_0 l \\exp\\left(-\\beta z_v \\right)
+    \int^\infty_0 l \exp\left(-\beta z_v \right)
     J_0(lr) dl =
-    \\frac{ z_v (\\gamma R + 1)}{R^3}\\exp\\left(-\\gamma R\\right)
+    \frac{ z_v (\gamma R + 1)}{R^3}\exp\left(-\gamma R\right)
 
 - ``j1_1`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 l^2 \\exp\\left(-al^2\\right) J_1(lr) dl =
-    \\frac{r}{4a^2} \\exp\\left(-\\frac{r^2}{4a}\\right)
+    \int^\infty_0 l^2 \exp\left(-al^2\right) J_1(lr) dl =
+    \frac{r}{4a^2} \exp\left(-\frac{r^2}{4a}\right)
 
 
 - ``j1_2`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 \\exp\\left(-al\\right) J_1(lr) dl =
-    \\frac{\\sqrt{a^2+r^2}-a}{r\\sqrt{a^2 + r^2}}
+    \int^\infty_0 \exp\left(-al\right) J_1(lr) dl =
+    \frac{\sqrt{a^2+r^2}-a}{r\sqrt{a^2 + r^2}}
 
 - ``j1_3`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 l \\exp\\left(-al\\right) J_1(lr) dl =
-    \\frac{r}{(a^2 + r^2)^{3/2}}
+    \int^\infty_0 l \exp\left(-al\right) J_1(lr) dl =
+    \frac{r}{(a^2 + r^2)^{3/2}}
 
 - ``j1_4`` [Chave_and_Cox_1982]_
 
   .. math::
 
-    \\int^\\infty_0 \\frac{l^2}{\\beta} \\exp\\left(-\\beta z_v \\right)
+    \int^\infty_0 \frac{l^2}{\beta} \exp\left(-\beta z_v \right)
     J_1(lr) dl =
-    \\frac{r(\\gamma R+1)}{R^3}\\exp\\left(-\\gamma R\\right)
+    \frac{r(\gamma R+1)}{R^3}\exp\left(-\gamma R\right)
 
 - ``j1_5`` [Chave_and_Cox_1982]_
 
   .. math::
 
-    \\int^\\infty_0 l^2 \\exp\\left(-\\beta z_v \\right)
+    \int^\infty_0 l^2 \exp\left(-\beta z_v \right)
     J_1(lr) dl =
-    \\frac{r z_v(\\gamma^2R^2+3\\gamma R+3)}{R^5}\\exp\\left(-\\gamma R\\right)
+    \frac{r z_v(\gamma^2R^2+3\gamma R+3)}{R^5}\exp\left(-\gamma R\right)
 
 Where
 
 .. math:: a >0, r>0
 .. math:: z_v = |z_{rec} - z_{src}|
-.. math:: R = \\sqrt{r^2 + z_v^2}
-.. math:: \\gamma = \\sqrt{2j\\pi\\mu_0f/\\rho}
-.. math:: \\beta = \\sqrt{l^2 + \\gamma^2}
+.. math:: R = \sqrt{r^2 + z_v^2}
+.. math:: \gamma = \sqrt{2j\pi\mu_0f/\rho}
+.. math:: \beta = \sqrt{l^2 + \gamma^2}
 
 
 Implemented Fourier transforms
@@ -161,43 +161,43 @@ Implemented Fourier transforms
 
   .. math::
 
-    \\int^\\infty_0 l\\exp\\left(-a^2l^2\\right) \\sin(lr) dl =
-    \\frac{\\sqrt{\\pi}r}{4a^3} \\exp\\left(-\\frac{r^2}{4a^2}\\right)
+    \int^\infty_0 l\exp\left(-a^2l^2\right) \sin(lr) dl =
+    \frac{\sqrt{\pi}r}{4a^3} \exp\left(-\frac{r^2}{4a^2}\right)
 
 - ``sin_2`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 \\exp\\left(-al\\right) \\sin(lr) dl =
-    \\frac{r}{a^2 + r^2}
+    \int^\infty_0 \exp\left(-al\right) \sin(lr) dl =
+    \frac{r}{a^2 + r^2}
 
 - ``sin_3`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 \\frac{l}{a^2+l^2} \\sin(lr) dl =
-    \\frac{\\pi}{2} \\exp\\left(-ar\\right)
+    \int^\infty_0 \frac{l}{a^2+l^2} \sin(lr) dl =
+    \frac{\pi}{2} \exp\left(-ar\right)
 
 - ``cos_1`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 \\exp\\left(-a^2l^2\\right) \\cos(lr) dl =
-    \\frac{\\sqrt{\\pi}}{2a} \\exp\\left(-\\frac{r^2}{4a^2}\\right)
+    \int^\infty_0 \exp\left(-a^2l^2\right) \cos(lr) dl =
+    \frac{\sqrt{\pi}}{2a} \exp\left(-\frac{r^2}{4a^2}\right)
 
 - ``cos_2`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 \\exp\\left(-al\\right) \\cos(lr) dl =
-    \\frac{a}{a^2 + r^2}
+    \int^\infty_0 \exp\left(-al\right) \cos(lr) dl =
+    \frac{a}{a^2 + r^2}
 
 - ``cos_3`` [Anderson_1975]_
 
   .. math::
 
-    \\int^\\infty_0 \\frac{1}{a^2+l^2} \\cos(lr) dl =
-    \\frac{\\pi}{2a} \\exp\\left(-ar\\right)
+    \int^\infty_0 \frac{1}{a^2+l^2} \cos(lr) dl =
+    \frac{\pi}{2a} \exp\left(-ar\right)
 
 
 """

empymod/scripts/tmtemod.py
@@ -76,105 +76,105 @@ We start with equation (105) in *Hun15*:
 
 .. math::
 
-    \hat{G}^{ee}_{xx}(\\boldsymbol{x}, \\boldsymbol{x'}, \omega) =
-    \hat{G}^{ee;i}_{xx;s}(\\boldsymbol{x}-\\boldsymbol{x'}, \omega)
-    + \\frac{1}{8\pi}\int^\infty_{\kappa=0}
-    \left(\\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s} -
-    \\frac{\zeta_s \\tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}\\right)
+    \hat{G}^{ee}_{xx}(\boldsymbol{x}, \boldsymbol{x'}, \omega) =
+    \hat{G}^{ee;i}_{xx;s}(\boldsymbol{x}-\boldsymbol{x'}, \omega)
+    + \frac{1}{8\pi}\int^\infty_{\kappa=0}
+    \left(\frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s} -
+    \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}\right)
     J_0(\kappa r)\kappa d \kappa
 
 .. math::
 
-    - \\frac{\cos(2\phi)}{8\pi}\int^\infty_{\kappa=0}
-    \left(\\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s} +
-    \\frac{\zeta_s \\tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}\\right)
+    - \frac{\cos(2\phi)}{8\pi}\int^\infty_{\kappa=0}
+    \left(\frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s} +
+    \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}\right)
     J_2(\kappa r)\kappa d \kappa .
 
 Ignoring the incident field, and using
-:math:`J_2 = \\frac{2}{\kappa r}J_1 - J_0` to avoid
+:math:`J_2 = \frac{2}{\kappa r}J_1 - J_0` to avoid
 :math:`J_2`-integrals, we get
 
 .. math::
 
-    \hat{G}^{ee}_{xx}(\\boldsymbol{x}, \\boldsymbol{x'}, \omega) =
-    \\frac{1}{8\pi}\int^\infty_{\kappa=0}
-    \left(\\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s}-
-    \\frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}\\right)
+    \hat{G}^{ee}_{xx}(\boldsymbol{x}, \boldsymbol{x'}, \omega) =
+    \frac{1}{8\pi}\int^\infty_{\kappa=0}
+    \left(\frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s}-
+    \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}\right)
     J_0(\kappa r)\kappa\,{\mathrm{d}}\kappa
 
 .. math::
 
-    + \\frac{\cos(2\phi)}{8\pi}\int^\infty_{\kappa=0}
-    \left(\\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s} +
-    \\frac{\zeta_s \\tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}\\right)
+    + \frac{\cos(2\phi)}{8\pi}\int^\infty_{\kappa=0}
+    \left(\frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s} +
+    \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}\right)
     J_0(\kappa r)\kappa\,{\mathrm{d}}\kappa
 
 .. math::
 
-    - \\frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
-    \left(\\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s} +
-    \\frac{\zeta_s \\tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}\\right)
+    - \frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
+    \left(\frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s} +
+    \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}\right)
     J_1(\kappa r)\,{\mathrm{d}}\kappa .
 
 From this the TM- and TE-parts follow as
 
 .. math::
 
-     {\mathrm{TE}} = \\frac{\cos(2\phi)-1}{8\pi}\int^\infty_{\kappa=0}
-     \\frac{\zeta_s \\tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}
+     {\mathrm{TE}} = \frac{\cos(2\phi)-1}{8\pi}\int^\infty_{\kappa=0}
+     \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}
      J_0(\kappa r)\kappa\,{\mathrm{d}}\kappa
-      - \\frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
-     \\frac{\zeta_s \\tilde{g}^{te}_{zz;s}}{\\bar{\Gamma}_s}
+      - \frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
+     \frac{\zeta_s \tilde{g}^{te}_{zz;s}}{\bar{\Gamma}_s}
      J_1(\kappa r)\,{\mathrm{d}}\kappa ,
 
 .. math::
 
-       {\mathrm{TM}} = \\frac{\cos(2\phi)+1}{8\pi}\int^\infty_{\kappa=0}
-     \\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s}
+       {\mathrm{TM}} = \frac{\cos(2\phi)+1}{8\pi}\int^\infty_{\kappa=0}
+     \frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s}
      J_0(\kappa r)\kappa\,{\mathrm{d}}\kappa
-     - \\frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
-     \\frac{\Gamma_s \\tilde{g}^{tm}_{hh;s}}{\eta_s}
+     - \frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
+     \frac{\Gamma_s \tilde{g}^{tm}_{hh;s}}{\eta_s}
      J_1(\kappa r)\,{\mathrm{d}}\kappa .
 
 Equations (108) and (109) in Hun15 yield the required parameters
-:math:`\\tilde{g}^{tm}_{hh;s}` and :math:`\\tilde{g}^{te}_{zz;s}`,
+:math:`\tilde{g}^{tm}_{hh;s}` and :math:`\tilde{g}^{te}_{zz;s}`,
 
 .. math::
 
-     \\tilde{g}^{tm}_{hh;s} = P^{u-}_s W^u_s + P^{d-}_s W^d_s ,
+     \tilde{g}^{tm}_{hh;s} = P^{u-}_s W^u_s + P^{d-}_s W^d_s ,
 
 .. math::
 
-     \\tilde{g}^{te}_{zz;s} = \\bar{P}^{u+}_s \\bar{W}^u_s +
-                              \\bar{P}^{d+}_s \\bar{W}^d_s \ .
+     \tilde{g}^{te}_{zz;s} = \bar{P}^{u+}_s \bar{W}^u_s +
+                              \bar{P}^{d+}_s \bar{W}^d_s \ .
 
 The parameters :math:`P^{u\pm}_s` and :math:`P^{d\pm}_s` are given in equations
-(81) and (82), :math:`\\bar{P}^{u\pm}_s` and :math:`\\bar{P}^{d\pm}_s` in
+(81) and (82), :math:`\bar{P}^{u\pm}_s` and :math:`\bar{P}^{d\pm}_s` in
 equations (A-8) and (A-9); :math:`W^u_s` and :math:`W^d_s` in equation (74)
 in Hun15. This yields
 
 .. math::
 
-     \\tilde{g}^{te}_{zz;s} =
-     \\frac{\\bar{R}_s^+}{\\bar{M}_s}\left\{\exp[-\\bar{\Gamma}_s(z_s-z+d^+)] +
-     \\bar{R}_s^-\exp[-\\bar{\Gamma}_s(z_s-z+d_s+d^-)]\\right\}
+     \tilde{g}^{te}_{zz;s} =
+     \frac{\bar{R}_s^+}{\bar{M}_s}\left\{\exp[-\bar{\Gamma}_s(z_s-z+d^+)] +
+     \bar{R}_s^-\exp[-\bar{\Gamma}_s(z_s-z+d_s+d^-)]\right\}
 
 .. math::
 
-     + \\frac{\\bar{R}_s^-}{\\bar{M}_s}
-     \left\{\exp[-\\bar{\Gamma}_s(z-z_{s-1}+d^-)]+
-     \\bar{R}_s^+\exp[-\\bar{\Gamma}_s(z-z_{s-1}+d_s+d^+)]\\right\} ,
+     + \frac{\bar{R}_s^-}{\bar{M}_s}
+     \left\{\exp[-\bar{\Gamma}_s(z-z_{s-1}+d^-)]+
+     \bar{R}_s^+\exp[-\bar{\Gamma}_s(z-z_{s-1}+d_s+d^+)]\right\} ,
 
 .. math::
 
-     =\\frac{\\bar{R}_s^+}{\\bar{M}_s}\left\{\exp[-\\bar{\Gamma}_s(2z_s-z-z')]
-     + \\bar{R}_s^-\exp[-\\bar{\Gamma}_s(z'-z+2d_s)]\\right\}
+     =\frac{\bar{R}_s^+}{\bar{M}_s}\left\{\exp[-\bar{\Gamma}_s(2z_s-z-z')]
+     + \bar{R}_s^-\exp[-\bar{\Gamma}_s(z'-z+2d_s)]\right\}
 
 .. math::
 
-     + \\frac{\\bar{R}_s^-}{\\bar{M}_s}
-     \left\{\exp[-\\bar{\Gamma}_s(z+z'-2z_{s-1})]+
-     \\bar{R}_s^+\exp[-\\bar{\Gamma}_s(z-z'+2d_s)]\\right\} ,
+     + \frac{\bar{R}_s^-}{\bar{M}_s}
+     \left\{\exp[-\bar{\Gamma}_s(z+z'-2z_{s-1})]+
+     \bar{R}_s^+\exp[-\bar{\Gamma}_s(z-z'+2d_s)]\right\} ,
 
 
 where :math:`d^\pm` is taken from the text below equation (67). There are four
@@ -187,23 +187,23 @@ If we look at TE+-, we have
 
 .. math::
 
-   \\tilde{g}^{te+-}_{zz;s} =
-   \\frac{\\bar{R}_s^+}{\\bar{M}_s}\exp[-\\bar{\Gamma}_s(2z_s-z-z')] \ ,
+   \tilde{g}^{te+-}_{zz;s} =
+   \frac{\bar{R}_s^+}{\bar{M}_s}\exp[-\bar{\Gamma}_s(2z_s-z-z')] \ ,
 
 
 and therefore
 
 .. math::
 
-   {\mathrm{TE}}^{+-} = \\frac{\cos(2\phi)-1}{8\pi}\int^\infty_{\kappa=0}
-   \\frac{\zeta_s \\bar{R}_s^+}{\\bar{\Gamma}_s\\bar{M}_s}
-   \exp[-\\bar{\Gamma}_s(2z_s-z-z')]
+   {\mathrm{TE}}^{+-} = \frac{\cos(2\phi)-1}{8\pi}\int^\infty_{\kappa=0}
+   \frac{\zeta_s \bar{R}_s^+}{\bar{\Gamma}_s\bar{M}_s}
+   \exp[-\bar{\Gamma}_s(2z_s-z-z')]
    J_0(\kappa r)\kappa\,{\mathrm{d}}\kappa
 
 .. math::
-   - \\frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
-   \\frac{\zeta_s \\bar{R}_s^+}{\\bar{\Gamma}_s\\bar{M}_s}
-   \exp[-\\bar{\Gamma}_s(2z_s-z-z')]
+   - \frac{\cos(2\phi)}{4\pi r}\int^\infty_{\kappa=0}
+   \frac{\zeta_s \bar{R}_s^+}{\bar{\Gamma}_s\bar{M}_s}
+   \exp[-\bar{\Gamma}_s(2z_s-z-z')]
    J_1(\kappa r)\,{\mathrm{d}}\kappa .
 
 We can compare this to equation (4.165) in Zio18, with :math:`\hat{I}^e_x=1`
@@ -211,24 +211,24 @@ and slightly re-arranging it to look more alike, we get
 
 .. math::
 
-   \hat{E}^{+-}_{xx;H} = \\frac{y^2}{4\pi r^2}
-   \int^\infty_{\kappa=0} \\frac{\zeta_1}{\Gamma_1}
-   \\frac{R^-_{H;1}}{M_{H;1}}
+   \hat{E}^{+-}_{xx;H} = \frac{y^2}{4\pi r^2}
+   \int^\infty_{\kappa=0} \frac{\zeta_1}{\Gamma_1}
+   \frac{R^-_{H;1}}{M_{H;1}}
    \exp(-\Gamma_1 h^{+-})J_0(\kappa r)\kappa d\kappa
 
 .. math::
 
-  + \\frac{x^2-y^2}{4\pi r^3}
-  \int^\infty_{\kappa=0} \\frac{\zeta_1}{\Gamma_1}
-  \left(\\frac{R^-_{H;1}}{M_{H;1}} -
-  \\frac{R^-_{H;1}(\kappa=0)}{M_{H;1}(\kappa=0)}\\right)
+  + \frac{x^2-y^2}{4\pi r^3}
+  \int^\infty_{\kappa=0} \frac{\zeta_1}{\Gamma_1}
+  \left(\frac{R^-_{H;1}}{M_{H;1}} -
+  \frac{R^-_{H;1}(\kappa=0)}{M_{H;1}(\kappa=0)}\right)
   \exp(-\Gamma_1 h^{+-})J_1(\kappa r) d\kappa
 
 
 .. math::
 
-   - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-   \\frac{R^-_{H;1}(\kappa=0)}{M_{H;1}(\kappa=0)}
+   - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+   \frac{R^-_{H;1}(\kappa=0)}{M_{H;1}(\kappa=0)}
    \exp(-\gamma_1 R^{+-}) .
 
 The notation in this equation follows Zio18.
@@ -246,44 +246,44 @@ notation of Zio18 (for the notation as in Hun15 see the implementation in
 
 .. math::
 
-  TE^{++} = + \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{\exp(-\gamma_1 |h^-|) }{M_{H;1}(\kappa=0)} ,
+  TE^{++} = + \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{\exp(-\gamma_1 |h^-|) }{M_{H;1}(\kappa=0)} ,
 
 .. math::
 
-  TE^{-+} = - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{R^+_{H;1}(\kappa=0)\exp(-\gamma_1 h^{-+}) }{M_{H;1}(\kappa=0)} ,
+  TE^{-+} = - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{R^+_{H;1}(\kappa=0)\exp(-\gamma_1 h^{-+}) }{M_{H;1}(\kappa=0)} ,
 
 .. math::
 
-  TE^{+-} = - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{R^-_{H;1}(\kappa=0)\exp(-\gamma_1 h^{+-}) }{M_{H;1}(\kappa=0)} ,
+  TE^{+-} = - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{R^-_{H;1}(\kappa=0)\exp(-\gamma_1 h^{+-}) }{M_{H;1}(\kappa=0)} ,
 
 .. math::
 
-  TE^{--} = + \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{R^+_{H;1}(\kappa=0)R^-_{H;1}(\kappa=0)\exp(-\gamma_1 h^{--}) }
+  TE^{--} = + \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{R^+_{H;1}(\kappa=0)R^-_{H;1}(\kappa=0)\exp(-\gamma_1 h^{--}) }
   {M_{H;1}(\kappa=0)} ,
 
 .. math::
 
-  TM^{++} = - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{\exp(-\gamma_1 |h^-|) }{M_{V;1}(\kappa=0)} ,
+  TM^{++} = - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{\exp(-\gamma_1 |h^-|) }{M_{V;1}(\kappa=0)} ,
 
 .. math::
 
-  TM^{-+} = - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{R^+_{V;1}(\kappa=0)\exp(-\gamma_1 h^{-+}) }{M_{V;1}(\kappa=0)} ,
+  TM^{-+} = - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{R^+_{V;1}(\kappa=0)\exp(-\gamma_1 h^{-+}) }{M_{V;1}(\kappa=0)} ,
 
 .. math::
 
-  TM^{+-} = - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{R^-_{V;1}(\kappa=0)\exp(-\gamma_1 h^{+-}) }{M_{V;1}(\kappa=0)} ,
+  TM^{+-} = - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{R^-_{V;1}(\kappa=0)\exp(-\gamma_1 h^{+-}) }{M_{V;1}(\kappa=0)} ,
 
 .. math::
 
-  TM^{--} = - \\frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
-  \\frac{R^+_{V;1}(\kappa=0)R^-_{V;1}(\kappa=0)\exp(-\gamma_1 h^{--}) }
+  TM^{--} = - \frac{\zeta_1 (x^2-y^2)}{4\pi\gamma_1 r^4}
+  \frac{R^+_{V;1}(\kappa=0)R^-_{V;1}(\kappa=0)\exp(-\gamma_1 h^{--}) }
   {M_{V;1}(\kappa=0)} .
 
 
@@ -301,7 +301,7 @@ can use
 
 .. math::
 
-   \cos(2\phi) = -\\frac{x^2-y^2}{r^2} \ .
+   \cos(2\phi) = -\frac{x^2-y^2}{r^2} \ .
 
 
 """

empymod/transform.py
@@ -124,7 +124,7 @@ def hqwe(zsrc, zrec, lsrc, lrec, off, factAng, depth, ab, etaH, etaV, zetaH,
 
     as a quadrature sum which form is similar to the DLF (equation 15),
 
-    .. math::   F_i   \\approx \sum^m_{j=1} f(x_j/r)w_j g(x_j) =
+    .. math::   F_i   \approx \sum^m_{j=1} f(x_j/r)w_j g(x_j) =
                 \sum^m_{j=1} f(x_j/r)\hat{g}(x_j) \ ,
 
     but with various bells and whistles applied (using the so-called Shanks

runtests.sh
@@ -2,7 +2,7 @@
 
 # Help text
 usage="
-$(basename "$0") [-hcpnd] [-v VERSION(S)]
+$(basename "$0") [-hcpndw] [-v VERSION(S)]
 
 Run pytest for empymod locally in an isolated venv before submitting to
 GitHub/Travis-CI; by default for all supported python versions of empymod.
@@ -14,6 +14,7 @@ where:
     -p : Print output of conda.
     -n : Run tests without numexpr/matplotlib/IPython.
     -d : Delete environments after tests
+    -w : Disable warnings
 
 "
 
@@ -27,6 +28,7 @@ STR2="**  WITH numexpr/matplotlib/IPython  "
 PROPS="--mpl --flake8"
 INST="pytest-flake8 pytest-mpl"
 SD="_soft-dep"
+WARN=""
 
 # Get Optional Input
 while getopts "hv:cpnd" opt; do
@@ -48,6 +50,8 @@ while getopts "hv:cpnd" opt; do
        SD=""
        ;;
     d) DELETE=true
+       ;;
+    w) WARN="--disable-warnings"
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -95,7 +99,7 @@ for i in ${PYTHON3VERSION[@]}; do
 
   # Run tests
   cp tests/matplotlibrc .
-  pytest --cov=empymod $PROPS
+  pytest --cov=empymod $PROPS $WARN
   rm matplotlibrc
 
   # De-activate venv