453a1d26559b117143a808c2489034c7f6adbca5
Dieter Werthmüller <mail@werthmuller.org>
2024-02-16 19:07:10 +0100

    linkcheck

--- examples/time_domain/tem_walktem.py
+++ examples/time_domain/tem_walktem.py
@@ -24,8 +24,7 @@ The incentive for this example came from Leon Foks (`@leonfoks
 <https://github.com/usgs/geobipy>`_, and it was created with his help and also
 the help of Seogi Kang (`@sgkang <https://github.com/sgkang>`_) from
 `simpegEM1D <https://github.com/simpeg/simpegem1d>`_; the waveform function is
-based on work from Kerry Key (`@kerrykey <https://github.com/kerrykey>`_) from
-`emlab <https://emlab.ldeo.columbia.edu>`_.
+based on work from Kerry Key (`@kerrykey <https://github.com/kerrykey>`_).
 """
 import empymod
 import numpy as np
