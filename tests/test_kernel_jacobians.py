f4f8d5699fa3a0c79da606fcd28ae1135c9d5c19
Wouter Deleersnyder <wouter.deleersnyder@student.kuleuven.be>
2024-02-19 22:24:52 +0100

    Rudimentary tests for Jacobian Rp and Rm.

--- tests/test_kernel_jacobians.py
+++ tests/test_kernel_jacobians.py
@@ -32,7 +32,7 @@ def test_reflections(njit):                                    # 3. reflections
     for _, val in dat.items():
         Rp, Rm = reflections(**val[0],debug=False)
         assert_allclose(Rp, val[1])
-        # assert_allclose(Rm, val[2]) #TODO: Debug Rm
+        assert_allclose(Rm, val[2])
 
 @pytest.mark.parametrize("njit", [False])
 def test_reflections_jacobian(njit):                                    # 3. reflections
@@ -40,12 +40,44 @@ def test_reflections_jacobian(njit):                                    # 3. ref
         reflections = kernel.reflections
     else:
         reflections = kernel.reflections.py_func
+    depth = np.r_[-np.inf, 0, 5, 20, 40, 100, 140]  # Layer boundaries
+    omega = 100
+    eps = 8.854187817e-12
+    mu_0 = 4 * np.pi * 1e-7
+    kappa = 1
+    cond = np.r_[1 / 2e14, 0.4, 1, 0.4, 1, 0.1, 0.01]
 
-    dat = DATAKERNEL['refl'][()]
-    for _, val in dat.items():
-        Rp, Rm, dRp, dRm= reflections(**val[0], ana_deriv=True)
-        assert_allclose(Rp, val[1])
-        assert_allclose(Rm, val[2])
+    e_zH = cond + 1j * omega * eps
+    e_zH = e_zH.reshape(1, -1)
+    z_eH = 1j * omega * mu_0 * np.ones_like(e_zH)
+    gamma2 = z_eH * e_zH
+    Gam = np.sqrt(kappa ** 2 + gamma2).reshape(1, 1, -1, 1)
+    Rp, Rm, dRp, dRm = reflections(depth, e_zH, Gam, 0, 6, ana_deriv=True, z_eH=z_eH, debug=True)
+
+
+    for cond_idx in np.arange(cond.size):
+        h = 1e-6
+
+        cond_pert = cond.copy()
+
+        cond_pert[cond_idx] = cond_pert[cond_idx] + h
+        e_zH = cond_pert + 1j * omega * eps
+        e_zH = e_zH.reshape(1, -1)
+        z_eH = 1j * omega * mu_0 * np.ones_like(e_zH)
+        gamma2 = z_eH * e_zH
+        Gam = np.sqrt(kappa ** 2 + gamma2).reshape(1, 1, -1, 1)
+        Rp_pert, Rm_pert = reflections(depth, e_zH, Gam, 0, 6, ana_deriv=False, z_eH=z_eH, debug=True)
+
+        cond_pert = cond.copy()
+        cond_pert[cond_idx] = cond_pert[cond_idx] -  h
+        e_zH = cond_pert + 1j * omega * eps
+        e_zH = e_zH.reshape(1, -1)
+        gamma2 = z_eH * e_zH
+        Gam = np.sqrt(kappa ** 2 + gamma2).reshape(1, 1, -1, 1)
+        Rp_pert2, Rm_pert2 = reflections(depth, e_zH, Gam, 0, 6, ana_deriv=False, z_eH=z_eH, debug=True)
+
+        assert_allclose(dRp[0, 0, :, 0, cond_idx], (Rp_pert - Rp_pert2).flatten() / (2*h), rtol=1e-6,atol=1e-9,) #TODO: Is this strict enough?
+        assert_allclose(dRm[0, 0, :, 0, cond_idx], (Rm_pert - Rm_pert2).flatten() / (2*h), rtol=1e-6,atol=1e-9,)
 
 def test_all_dir():
     assert set(kernel.__all__) == set(dir(kernel))
