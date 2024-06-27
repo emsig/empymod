6ef2b45b4bdfa6103845a7c79212c47c5199f81d
Dieter Werthmüller <prisae@users.noreply.github.com>
2024-06-27 14:26:44 +0200

    Fix coveralls reporting again (#216)

--- empymod/transform.py
+++ empymod/transform.py
@@ -1210,7 +1210,7 @@ def get_dlf_points(filt, inp, nr_per_dec):
 
     # Get pts_per_dec and define number of out-values, depending on pts_per_dec
     if nr_per_dec < 0:  # Lagged Convolution DLF
-        pts_per_dec = 1/np.log(filt.factor)
+        pts_per_dec = np.squeeze(1/np.log(filt.factor))
 
         # Calculate number of output values
         nout = int(np.ceil(np.log(outmax/outmin)*pts_per_dec) + 1)
